f0ff015a18f2f3fff265f58816af6fb85755d061
Catherine Meyer <deathcat05@catherines-mbp.attlocal.net>
2017-11-08 18:57:00 -0800

    put it all into main

--- HW3/HW3/main.c
+++ HW3/HW3/main.c
@@ -1,15 +1,65 @@
-//
-//  main.c
-//  HW3
-//
-//  Created by Catherine Meyer on 11/7/17.
-//  Copyright © 2017 Catherine Meyer. All rights reserved.
-//
 
+//#include "sudoku.h"
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include <pthread.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+
+#define NUMTHREADS 27
+#define MAXNUM 81
+#define MAXROW 9
+#define MAXCOLUMN 9
+
+typedef struct
+{
+    int row;
+    int column;
+    int threadNumber;
+} parameters;
+
+void *validateRow(void *sudokuTable);
+void *validateColumn(void *sudokuTable);
+void *validateGrid(void *sudokuTable);
+
+int main(void)
+{
+    //File readin operations
+    FILE *file;
+    int sudokuPuzzle[9][9];
+    int i,j,count = 0;
+    
+    int value[MAXNUM];
+    
+    file = fopen("sudoku.txt", "r");
+    
+    if (file == NULL)
+    {
+        printf("Error: file pointer is null.");
+        exit(1);
+    }
+    
+    for( i = 0; i < MAXROW; i++ )
+    {
+        for( j = 0; j < MAXCOLUMN; j++ )
+        {
+            fscanf(file, " %d", &value[count]);
+            sudokuPuzzle[i][j] = value[count];
+            //printf("element %d is %d\n",count,  sudokuPuzzle[i][j]);
+            count++;
+            
+        }
+    }
+
+    pthread_t thread[NUMTHREADS];
+    
+    for(int i =0; i < MAXROW; i++)
+    {
+        for(int j =0; j < MAXCOLUMN; j++)
+        {
+            pthread_create(thread, NULL, validateRow, sudokuPuzzle);
+        }
+    }
+    
 }
+
--- HW3/HW3/sudoku.c
+++
@@ -1,9 +0,0 @@
-//
-//  sudoku.c
-//  HW3
-//
-//  Created by Catherine Meyer on 11/7/17.
-//  Copyright © 2017 Catherine Meyer. All rights reserved.
-//
-
-#include "sudoku.h"
