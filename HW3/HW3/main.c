17abb941946938422a2d38697a095a0ccf476c8f
Catherine Meyer <deathcat05@Catherines-MacBook-Pro.local>
2017-11-15 10:12:19 -0800

    THREADS WORK. Output of which rows, cols, grids, are invalid DOES NOT WORK

--- HW3/HW3/main.c
+++ HW3/HW3/main.c
@@ -1,38 +1,38 @@
-#include <stdlib.h>
-#include <string.h>
+/*
+ * This is a multiethreaded suduko validator program created using pthreads.
+ */
+
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
+#define NUMTHREADS 27 //number of threads in the program
 
 
-#define NUMTHREADS 27
-#define MAXNUM 81
-#define MAXROW 9
-#define MAXCOLUMN 9
-//The struct for the threads you
+/* structure for passing data to threads*/
 typedef struct
 {
-    // pthread_t threadID;
     int row;
     int column;
-    int threadCount;
-    int sudokuPuzzle[9][9];
-    
+    int (*puzzle)[9];
+    int valid[NUMTHREADS];
+    int invalidCase;
 } parameters;
 
-void *validateRow(void *sudokuTable);
-void *validateColumn(void *sudokuTable);
-void  *validateGrid(void *sudokuTable);
 
-int main(void)
+void* validateRow(void* sudokuData);
+void* validateColumn(void* sudokuData);
+void* validateGrid(void* sudokuData);
+
+// method to validate that each column has numbers 1-9
+int main()
 {
-    //parameters *puzzle = (parameters*) malloc(sizeof(parameters));
-    
-    //File readin operations
-    FILE *file;
     int puzzle[9][9];
-    int i,j,count = 0;
+    int threadNum = 0;
+    pthread_t* thread;
+    thread = malloc(NUMTHREADS*sizeof(pthread_t));
+    parameters *data = (parameters*) malloc(sizeof(parameters));
     
-    file = fopen("sudoku.txt", "r");
+    FILE *file = fopen("sudoku.txt", "r");
     
     if (file == NULL)
     {
@@ -40,111 +40,161 @@ int main(void)
         exit(1);
     }
     
-    for( i = 0; i < MAXROW; i++ )
+    for( int i = 0; i < 9; i++ )
     {
-        for( j = 0; j < MAXCOLUMN; j++ )
+        for( int j = 0; j < 9; j++ )
         {
             fscanf(file, " %d", &puzzle[i][j]);
-            //printf("%d\n", puzzle[i][j]); //This is just for test purposes DELETE BEFORE SUBMITTING!
-            count++;
-            
         }
     }
     fclose(file);
     
     
-    //Establishing the thread(s) for interaction
+    //print suduko after reading DELETE BEFORE SUBMISSION
+    printf("The given solution is:\n");
+    for(int i=0; i<9; i++){
+        for(int j=0; j<9; j++){
+            printf("%d ", puzzle[i][j]);
+        }
+        printf("\n");
+    }
     
-    pthread_t* thread;
-    thread = malloc(NUMTHREADS*sizeof(pthread_t));
-    int threadNum = 0;
-    for(threadNum; threadNum < NUMTHREADS; threadNum++)
+    for (int i = 0; i < 9; i++)
     {
-    for(int i = 0; i < MAXROW; i++)
-    {
-        for(int j =0; j < MAXCOLUMN; j++)
+        for (int j = 0; j < 9; j++)
         {
-            //printf("%d", puzzle[i][j]); //JUST TEST DATA. DELETE BEFORE SUBMISSION!
-            if((i%3 == 0) && (j%3 == 0)) //Establishing 3x3 grid
+            //threads to check 3X3 squares
+            if (i%3 == 0 && j%3 == 0)
             {
-                
                 parameters *gridData = (parameters*) malloc(sizeof(parameters));
-                gridData->row = i; //Assigning the i value to row of the struct
-                gridData->column = j; //Assigning the j value to the column of the struct
-                gridData->sudokuPuzzle[i][j] = puzzle[i][j];
-                //pthread_t thread = gridData->threadID;
-                gridData->threadCount = threadNum;
-                // pthread_create(&thread[threadNum++], NULL, validateGrid, gridData); //Creating the subsections threads
+                gridData->row = i;
+                gridData->column = j;
+                gridData->puzzle = &puzzle[9];
+                gridData->invalidCase =0;
+                pthread_create(&thread[threadNum++], NULL, validateGrid, gridData);
             }
-            if(j == 0)
+            //threads to check coloumn
+            if(i==0)
             {
-                parameters *rowData = (parameters*) malloc(sizeof(parameters));
-                rowData->row =i;
+                parameters *colData = (parameters *) malloc(sizeof(parameters));
+                colData->row = i;
+                colData->column = j;
+                colData->puzzle = &puzzle[9];
+                colData->invalidCase =0;
+                pthread_create(&thread[threadNum++], NULL, validateColumn, colData);
+            }
+            //threads to check row
+            if(j==0)
+            {
+                parameters *rowData = (parameters *) malloc(sizeof(parameters));
+                rowData->row = i;
                 rowData->column = j;
-                rowData->sudokuPuzzle[i][j] = puzzle[i][j];
-                rowData->threadCount = threadNum;
-                pthread_create(&thread[threadNum], NULL, validateRow, rowData);
-                
+                rowData->puzzle = &puzzle[9];
+                rowData->invalidCase =0;
+                pthread_create(&thread[threadNum++], NULL, validateRow, rowData);
             }
         }
     }
-    }
-    for(threadNum; threadNum < NUMTHREADS; threadNum++)
+    
+    //waiting for all threads to finish
+    for (int threadNum = 0; threadNum < NUMTHREADS; threadNum++)
     {
         pthread_join(thread[threadNum], NULL);
-    }
-    
-}
-
-void *validateGrid(void *sudokuPuzzle)
-{
-    parameters *grid = (parameters*) sudokuPuzzle;
-    int row = grid->row;
-    int col = grid->column;
-    int num = 0;
-    int arrayToCheck[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //The array used to check
-    
-    for(int i = row; i < (row+3); i++)
-    {
-        for(int j = col; j < (col+3); j++)
+        if(data->valid[threadNum] == 0 && data->invalidCase==1)
         {
-            
-            num = grid->sudokuPuzzle[i][j];
-            if(arrayToCheck[num-1] == 1)
-            {
-                printf("Error in grid");
-            }
-            else
-            {
-                arrayToCheck[num-1] = 1;
-                printf("grids are valid");
-                
-            }
+            printf("Row %d doesn't have the required values.\n", data->row);
         }
+        printf("THe solution is invalid\n");
+
     }
+    
+    printf("Valid Solution\n");
     return 0;
 }
 
-void *validateRow(void *sudokuPuzzle)
+void *validateRow(void *sudokuData)
 {
-    parameters *rows = (parameters*) sudokuPuzzle;
-    int row = rows->row;
-    int column = rows->column;
-    int checkArray[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0,0};
+    parameters *rowData = (parameters*) sudokuData;
+    int row = rowData->row;
+    int checkRowArray[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
     
-    for(int i = 0; i < MAXROW; i++)
+    //check if row contains all number from 1-9, if not then set the Crow array to be 0
+    //in order to check for all numbers. then set the valid array to 1 if row is valid.
+    for(int i = 0; i < 9 ; i++)
     {
-        int num = rows->sudokuPuzzle[row][i];
-        if(checkArray[num] ==1)
+        int num = rowData->puzzle[row][i];
+        if((num<1) || (num>9) || (checkRowArray[num - 1] == 1))
         {
-            printf("Error in row %d\n", row);
+            rowData->valid[row] = 0;
         }
         else
         {
-            checkArray[num] = 1;
-            printf("No errors in row!\n");
-            
+            checkRowArray[num-1] = 1;
         }
     }
-    return 0;
+    //set the valid array starting from 10-18
+    rowData->valid[9 + row] = 1;
+    pthread_exit(NULL);
 }
+
+// method to validate that each square has numbers 1-9
+void *validateGrid(void* sudokuData)
+{
+    parameters *grid = (parameters*) sudokuData;
+    int row = grid->row;
+    int column = grid->column;
+    int checkArrayGrid[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    
+    //check if squares contain all number from 1-9, if not then set the csqua array to be 0
+    //in order to check for all numbers. then set the valid array to 1 if row is valid.
+    for (int i=row; i < row + 3; i++)
+    {
+        for (int j=column; j < column + 3; j++)
+        {
+            int num = grid->puzzle[i][j];
+            if (num < 1 || num > 9 || checkArrayGrid[num - 1] == 1)
+            {
+                //grid->invalidCase = grid+1;
+            }
+            else
+            {
+                checkArrayGrid[num - 1] = 1;
+            }
+        }
+    }
+    //set the valid array starting from 1-9
+    grid->valid[row+column/3] = 1;
+    pthread_exit(NULL);
+}
+
+
+// method to validate that each column has numbers 1-9
+void *validateColumn(void *sudokuData)
+{
+    parameters *columns = (parameters*) sudokuData;
+    int column = columns->column;
+    int checkColumnArray[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    
+    //check if column contains all number from 1-9, if not then set the Ccol array to be 0
+    //in order to check for all numbers. then set the valid array to 1 if col is valid.
+    for(int i=0; i < 9 ; i++)
+    {
+        int num = columns->puzzle[i][column];
+        if((num < 1) || (num > 9) || (checkColumnArray[num - 1] == 1))
+        {
+            pthread_exit(NULL);
+        }
+        else
+        {
+            checkColumnArray[18+column] = 1;
+        }
+    }
+    
+    //set the valid array starting from 19-27
+    
+    columns->valid[18 + column] = 1;
+    pthread_exit(NULL);
+}
+
+
